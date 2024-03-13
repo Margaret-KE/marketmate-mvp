3a95291d34c609d2745b0ed58de0f9ff2f259d95
QueenMaggie <wanjirumargaret394@gmail.com>
2024-03-12 20:35:25 -0700

    updated

---
+++ config.py
@@ -0,0 +1,10 @@
+DATABASE_HOST = 'localhost'
+DATABASE_PORT = 3306
+DATABASE_USER = 'margaret'
+DATABASE_PASSWORD = 'Wa2024'
+DATABASE_NAME = 'marketmate'
+
+class Config:
+    SECRET_KEY = 'your_secret_key_here'
+    SQLALCHEMY_DATABASE_URI = 'mysql://username:password@localhost/db_name'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
