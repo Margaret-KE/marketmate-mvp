93d39c4b4f1edb1796a3c0127ed916f904668dd7
Margaret-KE <wanjirumargaret394@gmail.com>
2024-03-17 13:45:14 +0000

    updated

--- config.py
+++ config.py
@@ -8,10 +8,16 @@ load_dotenv()
 DB_USERNAME = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
-DATABASE_HOST = 'localhost'
-DATABASE_PORT = 3306
+DATABASE_HOST = ('LOCALHOST')
+DATABASE_PORT = (3306)
 
 class Config:
+    DEBUG = True
+    SECRET_KEY = 'your_secret_key_here'
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'username'
+    MYSQL_PASSWORD = 'password'
+    MYSQL_DB = 'database_name'
     SECRET_KEY = 'your_secret_key_here'
     SQLALCHEMY_DATABASE_URI = 'mysql://username:password@localhost/db_name'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
