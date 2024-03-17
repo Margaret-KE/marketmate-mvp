6b897f2ef0b91b4c7cb2310736eb9bacc0bdb641
Margaret-KE <wanjirumargaret394@gmail.com>
2024-03-17 12:53:27 +0000

    updated

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get database credentials from environment variables
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
 DATABASE_HOST = 'localhost'
 DATABASE_PORT = 3306
-DATABASE_USER = 'margaret'
-DATABASE_PASSWORD = 'Wa2024'
-DATABASE_NAME = 'marketmate'
 
 class Config:
     SECRET_KEY = 'your_secret_key_here'
