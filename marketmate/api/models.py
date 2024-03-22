cf53d1051977a1b8aa87a3ccd519c73fe44792c1
Margaret-KE <wanjirumargaret394@gmail.com>
2024-03-22 10:39:44 -0700

    models

--- marketmate/api/models.py
+++ marketmate/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# marketmate/api/models.py
+
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.username
+
