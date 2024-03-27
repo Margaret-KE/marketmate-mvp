ed23122d08ea5d84fd5e9ac99864372398cfa364
Margaret-KE <wanjirumargaret394@gmail.com>
2024-03-27 08:29:29 -0700

    models.py

--- marketmate/api/models.py
+++ marketmate/api/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-# marketmate/api/models.py
-
-from django.db import models
-
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        app_label = 'api'  # Explicitly specify the app label
 
     def __str__(self):
         return self.name
@@ -14,6 +13,9 @@ class Product(models.Model):
 class User(models.Model):
     username = models.CharField(max_length=100, unique=True)
     email = models.EmailField()
+
+    class Meta:
+        app_label = 'api'  # Explicitly specify the app label
 
     def __str__(self):
         return self.username
